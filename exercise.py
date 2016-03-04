11f07a1d41ec933a06d064a4772f969658004829
kater666 <michal.dolbniak@gmail.com>
2016-03-04 13:49:32 +0100

    Fixed problem

--- exercise.py
+++ exercise.py
@@ -4,16 +4,23 @@ def print_file(file):
     f = open(file, 'r')
     lines = f.readlines()
     my_list = []
-    print(lines)
     x = ''
     for i in lines:
-        if i.isnumeric() == True:
-            x += i
-        else:
-            my_list.append(x)
-            x = ''
+        for j in i:
+            if j.isnumeric() == True:
+                x += j
+            else:
+                if x == '':
+                    pass
+                else:
+                    my_list.append(x)
+
+                x = ''
 
     f.close()
     return my_list
 
-print(print_file(name))
+print_file(name)
+
+
+# Ugly but works
