35883279de52a3bf82fdf80b1f9d86590bece975
kater666 <michal.dolbniak@gmail.com>
2016-03-06 14:25:22 +0100

    Finished readCollumnNumber

--- Basics/readCollumnNumber.py
+++ Basics/readCollumnNumber.py
@@ -5,19 +5,16 @@ def read_column_number(file):
     f = open(file, 'r')
     lines = f.readlines()
     new_list = []
+    
     for i in lines:
         new_list.append(i[:-1])
-    #print(new_list)
+
     for i in range(0, len(new_list)):
         list = new_list[i].split(' ')
-        print(list)
-        if list[1] == 'Error:':
-            z = list[2:]
+        if (len(list)) > 1 and list[1] == 'Error:':
+            z = list[2:-1]
             print("Line %s Error type: %s" % (list[0], ' '.join(z)))
-            print("Time: %s" % list[1]) # daj mnie dane z następnej iteracji
+            print("Time: %s" % list[-1])
     f.close()
 
-
 read_column_number(file_name)
-
-# Not finished
